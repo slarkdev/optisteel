c2015f262ec58dc5ec6ea6b22581fa0bf2f00169
lesly ttito <lesly.ttito@gmail.com>
2025-10-07 08:29:29 -0500

    primera vista de analisis

--- src/app/home/analisis/analisis.component.ts
+++ src/app/home/analisis/analisis.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiLotesService } from '../../services/lote.service';
+import { ApiProyectosService } from '../../services/proyectos.service';
+import { Subject, takeUntil } from 'rxjs';
+import { Proyecto } from '../../models/proyecto';
+import { Lote } from '../../models/lote';
 
 @Component({
   selector: 'app-analisis',
   templateUrl: './analisis.component.html',
   styleUrl: './analisis.component.scss',
-  standalone: false
+  standalone: false,
 })
-export class AnalisisComponent {
+export class AnalisisComponent implements OnInit {
+  subscription = new Subject<void>();
+  proyectoSeleccionado: any;
+  loteSeleccionado: any;
+  constructor(
+    private apiLoteService: ApiLotesService,
+    private apiProyectoService: ApiProyectosService
+  ) {}
 
+  ngOnInit(): void {
+    this.apiProyectoService
+      .getProyectoSeleccionado()
+      .pipe(takeUntil(this.subscription))
+      .subscribe((proyecto) => {
+        this.proyectoSeleccionado = proyecto;
+      });
+
+    this.apiLoteService
+      .getLoteSeleccionado()
+      .pipe(takeUntil(this.subscription))
+      .subscribe((lote) => {
+        this.loteSeleccionado = lote;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.next();
+    this.subscription.complete();
+    console.log('🧹 analisis destruido');
+  }
 }
